d745337b0a6a0e8e98ca59ffe3569d3ec65329f0
Tsukuru Suzuki <suzukitsukuru@Tsukuru-Suzuki-MacBook-Pro.local>
2021-11-12 23:14:53 +0900

    editUserIdColumnInteger

--- db/migrate/20210917062754_change_data_title_to_qns.rb
+++ db/migrate/20210917062754_change_data_title_to_qns.rb
@@ -1,6 +1,6 @@
 class ChangeDataTitleToQns < ActiveRecord::Migration[6.1]
   def change
-    change_column :qns, :user_id, :integer
+    change_column :qns, :user_id, "integer USING CAST(user_id AS integer)"
     change_column :users, :name, :string
     change_column :users, :password, :string
   end
